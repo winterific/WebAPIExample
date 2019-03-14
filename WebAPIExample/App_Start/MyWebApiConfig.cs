1e86ac91a6361630d40f8c1c1f04bd7d041c7ed5
Winter, Samuel <s.winter@snhu.edu>
2019-03-14 09:03:03 -0400

    First

---
+++ WebAPIExample/App_Start/MyWebApiConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace WebAPIExample.App_Start
+{
+    public class MyWebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            config.Routes.MapHttpRoute(
+                name: "MyDefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { controller="My", id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WebAPIExample/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+using WebAPIExample.App_Start;
+
+namespace WebAPIExample
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            GlobalConfiguration.Configure(MyWebApiConfig.Register);
+
+        }
+    }
+}
